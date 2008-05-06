9fc20957c0e302f5ad589788894535d6fde2759f
linyupark@gmail.com <linyupark@gmail.com@382f9d10-0d48-0410-ac63-8f6eca9f32e0>
2008-05-06 07:33:24 +0000

    已删除的文件/文件夹

--- application/info/views/helpers/editor.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	class Zend_View_Helper_editor
-	{
-		function editor($content_id = 'content', $width='650', $height='480')
-		{
-			return '<iframe src="/static/editor/sina/Edit/editor.htm?id='.$content_id.'&ReadCookie=0" frameBorder="0" marginHeight="0" marginWidth="0" scrolling="No" width="'.$width.'" height="'.$height.'"></iframe>';
-		}
-	}
